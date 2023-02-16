485f25febcae8057d679645ec26f89a85d6df077
Svork7 <svork@ya.ru>
2023-02-16 11:29:38 +0300

    1.4. done

---
+++ 1/1.4/index.js
@@ -0,0 +1,15 @@
+//задача 1
+let name
+let admin
+name = 'Джон'
+admin = name
+alert(admin)
+
+//Задача 2
+let ourPlanetName = 'Земля'
+let currentUserName = 'Джон'
+
+// задача 3
+const BIRTHDAY = '18.04.1982' // можно использовать заглавные буквы, так как день рождения не меняется
+
+const AGE = someCode(BIRTHDAY) // нельзя использовать заглавные, так как возраст динамически вычисляется
