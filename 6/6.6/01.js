018a4c6a804457b398d8a243499f3c31f5b16575
Svork7 <svork@ya.ru>
2023-03-12 22:01:00 +0300

    6.6 done

---
+++ 6/6.6/01.js
@@ -0,0 +1,14 @@
+// задача 1
+function makeCounter() {
+  let count = 0
+
+  function counter() {
+    return count++
+  }
+
+  counter.set = (value) => (count = value)
+
+  counter.decrease = () => count--
+
+  return counter
+}
